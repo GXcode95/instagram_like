139e39a44780972114fb8e4e40d5ce3aaf5f247b
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-30 21:13:50 +0100

    prepare deploy

--- src/components/Github/index.jsx
+++ src/components/Github/index.jsx
@@ -5,7 +5,7 @@ import './style.scss'
 const Github = () => {
   return (
     <a href="https://github.com/GXcode95/instagram_like" className='my-github'>
-      <GitHubIcon sx={{fontSize:"50px", color: "rgba(0,0,0,0.7)"}}/>      
+      <GitHubIcon sx={{fontSize:"50px", color: "rgba(248, 130, 219, 0.45)"}}/>      
     </a>
   )
 }
